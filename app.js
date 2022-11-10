a52484a70a2719e4eb5fdbe74d8f790ea6eff5fa
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-11-10 17:51:31 +0100

    add functions to create and add note

--- app.js
+++ app.js
@@ -23,5 +23,46 @@ const closePanel = () => {
   category.selectedIndex = 0;
 };
 
+const addNote = () => {
+  if (
+    textArea.value !== "" &&
+    category.options[category.selectedIndex].value !== "0"
+  ) {
+    createNote();
+    error.style.visibility = "hidden";
+  } else {
+    error.style.visibility = "visible";
+  }
+};
+
+const createNote = () => {
+  const newNote = document.createElement("div");
+  newNote.classList.add("note");
+  newNote.setAttribute("id", cardID);
+
+  newNote.innerHTML = `
+    <div class="note-header">
+        <h3 class="note-title">${selectedValue}</h3>
+        <button class="delete-note">
+            <i class="fas fa-times icon"></i>
+        </button>
+    </div>
+    <div class="note-body">
+        ${textArea.value}
+    </div>
+    `;
+
+  noteArea.appendChild(newNote);
+  cardID++;
+  textArea.value = "";
+  category.selectedIndex = 0;
+  notePanel.style.display = "none";
+};
+
+const selectValue = () => {
+  selectedValue = category.options[category.selectedIndex].text;
+};
+
 addBtn.addEventListener("click", openPanel);
 cancelBtn.addEventListener("click", closePanel);
+saveBtn.addEventListener("click", addNote);
